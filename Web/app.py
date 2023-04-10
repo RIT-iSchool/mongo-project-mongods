b3e53fe9692ac42097967df0ccbd2a383f2981ec
Amin Shakib <ah5584@rit.edu>
2023-04-10 18:31:53 -0400

    Add post page

--- Web/app.py
+++ Web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
@@ -16,6 +17,15 @@ def search_posts():
     regex_query = { "$regex": ".*" + query + ".*", "$options": "i" }
     results = list(db.posts.find({ "description": regex_query }))
     return render_template('results.html', posts=results)
+
+@app.route('/show_post/<post_id>')
+def show_post(post_id):
+    post = db.posts.find_one({ '_id': ObjectId(post_id) })
+    if post:
+        return render_template('post.html', post=post)
+    else:
+        return "Post not found."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
