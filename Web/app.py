f79585c8f24eb5dd33c9c1686b68c9a5b4109844
Amin Shakib <ah5584@rit.edu>
2023-04-10 18:38:41 -0400

    Add images to posts

--- Web/app.py
+++ Web/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from pymongo import MongoClient
 from bson import ObjectId
+import gridfs
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['project']
 posts = db['posts']
+fs = gridfs.GridFS(db)
 
 @app.route('/')
 def home():
@@ -20,12 +22,22 @@ def search_posts():
 
 @app.route('/show_post/<post_id>')
 def show_post(post_id):
-    post = db.posts.find_one({ '_id': ObjectId(post_id) })
+    post = db.posts.find_one({ 'post_id': post_id })
     if post:
-        return render_template('post.html', post=post)
+        filename = 'test/' + str(post_id) + '.jpg'
+        image = fs.find_one({"filename": filename})
+        if image:
+            return render_template('post.html', post=post, image=image)
+        else:
+            return render_template('post.html', post=post)
     else:
         return "Post not found."
 
+
+@app.route('/download_image/<image_id>')
+def download_image(image_id):
+    image = fs.get(ObjectId(image_id))
+    return send_file(image, mimetype='image/jpg')
 
 if __name__ == '__main__':
     app.run(debug=True)
